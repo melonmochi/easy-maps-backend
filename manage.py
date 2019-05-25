43260859b16e6a410bcee89c54ac125368b449ea
melonmochi <melonmochi3@gmail.com>
2019-05-25 12:25:40 +0200

    Create Dockerfile

---
+++ manage.py
@@ -0,0 +1,9 @@
+from flask_script import Server, Manager
+from app import create_app
+
+app = create_app()
+manager = Manager(app)
+manager.add_command("runserver", Server(host='0.0.0.0'))
+
+if __name__ == "__main__":
+    manager.run()
