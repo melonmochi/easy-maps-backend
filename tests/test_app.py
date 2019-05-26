52cafc0410766f12b15f8def045763c9fe47702e
melonmochi <melonmochi3@gmail.com>
2019-05-26 15:11:45 +0200

    Fixed test error

--- tests/test_app.py
+++ tests/test_app.py
@@ -20,4 +20,4 @@ def test_hello():
     response = create_app().test_client().get('/')
 
     assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    assert response.data == b'Hello World!'
